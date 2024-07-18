04fb71245d327e35f7e1aabeacb2681270913727
Gilberto-GZ <ggz2489@hotmail.com>
2024-07-18 17:10:44 -0600

    Project Manager Added

--- classes/ProjectsManager.ts
+++ classes/ProjectsManager.ts
@@ -9,15 +9,35 @@ export class ProjectsManager {
     }
 
     newProject(data: IProject) {
+        const projectNames = this.list.map((project) => {
+            return project.name
+        })
+        const nameInUse = projectNames.includes(data.name)
+        if (nameInUse) {
+            throw new Error(`Project name "${data.name}" already in use`)
+        }
         const project = new Project(data)
         this.ui.append(project.ui)
         this.list.push(project)
         return project
     }
 
-    getProject() { }
+    getProject(id:string) { 
+        const project = this.list.find((project) => {
+            return project.id === id
+        })
+        return project
+    }
 
-    deleteProject() { }
+    deleteProject(id:string) {
+        const project = this.getProject(id)
+        if (!project) { return }
+        project.ui.remove()
+        const remaining = this.list.filter((project) => {
+            return project.id !== id
+        })
+        this.list = remaining
+     }
 
     exportToJSON() { }
 
--- src/index.ts
+++ src/index.ts
@@ -43,13 +43,14 @@ if (projectForm && projectForm instanceof HTMLFormElement) {
             userRole: formData.get("userRole") as UserRole,
             finishDate: new Date(formData.get("finishDate") as string)
         }
-
-        const project = projectsManager.newProject(projectData)
-        projectForm.reset()
-        closeModal("new-project-modal")
-    
-}) 
-
+        try {
+            const project = projectsManager.newProject(projectData)
+            projectForm.reset()
+            closeModal("new-project-modal")
+        } catch(err) {
+            alert(err)
+        }    
+    }) 
 } else {
     console.warn("New project form was not found. Check the ID!")
 }
